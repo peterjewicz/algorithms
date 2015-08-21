445b2f7095457aa76d95b587fde7fe1c97d0789f
Peterjewicz <peterjewicz@hotmail.com>
2015-08-21 00:28:43 -0500

    Unix timestamp comparision php

---
+++ php/compareUnixTime.php
@@ -0,0 +1,40 @@
+<?php
+function compareTime($time, $currentTime) //calculates the difference between two unix timestampos
+	{
+		if($time > $currentTime)
+		{
+			return "Time Error";
+		}
+		$days = 0;
+		$hours = 0;
+		$minutes =0;
+		$seconds =0;
+		$timeDifference = $currentTime - $time;
+		while($timeDifference - 86400 > 0)
+		{
+			$days++;
+			$timeDifference = $timeDifference - 86400;
+		}
+		while($timeDifference - 3600 > 0)
+		{
+			$hours++;
+			$timeDifference = $timeDifference - 3600;
+		}
+		while($timeDifference - 60 > 0)
+		{
+			$minutes++;
+			$timeDifference = $timeDifference - 60;
+		}
+		$seconds = $timeDifference ." Seconds ";
+		if($days > 0){$days = $days . " Days ";}
+			else{$days ="";}
+		if($hours > 0){$hours = $hours . " Hours ";}
+			else{$hours ="";}
+		if($minutes > 0){$minutes = $minutes . " Minutes ";}
+			else{$minute="";}
+	
+		$time = $days . $hours . $minutes . $seconds;
+		echo($time);
+		return $time;
+	}
+	?>
